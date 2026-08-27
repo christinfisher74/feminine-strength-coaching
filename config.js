c67ebd04a74f853146aa7b2264508efadf160a87
christinfisher74 <digitalchristin@gmail.com>
2026-08-27 10:31:37 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 window.FSC_CONFIG = {
   SUPABASE_URL: 'https://mimaexschnbsdnfcluev.supabase.co',
-  // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pbWFleHNjaG5ic2RuZmNsdWV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA1ODcyNTgsImV4cCI6MjA5NjE2MzI1OH0.Z6wnsi0u0lXJdrEuefJFmeaHCNROnSRs7z6EZclphrE
-  // NEVER put a service-role or secret key in this file.
-  SUPABASE_PUBLISHABLE_KEY: ''
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_9ZfkFDRoFbZF86eKY975-A_go2BoJ9o'
 };
